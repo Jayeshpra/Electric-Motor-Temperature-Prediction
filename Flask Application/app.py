72b9b3b39ce39afbf33b0cc5b736dce9f3ecd391
Jayesh Prajapati <prajapatijayesh369@gmail.com>
2025-08-06 04:11:06 +0530

    Update app.py

--- Flask Application/app.py
+++ Flask Application/app.py
@@ -4,23 +4,31 @@ import joblib
 
 app = Flask(__name__)
 model = joblib.load("Model Building\decision_tree_model.save")
-trans = joblib.load("Model Building\mm_scaler.save")
+x_trans = joblib.load("Model Building\mm_x_scaler.save")
+y_trans = joblib.load("Model Building\mm_y_scaler.save")
 
 app = Flask(__name__)
 
 @app.route('/')
 def predict():
-    return render_template('Manual_prediict.html')
+    return render_template('Manual_predict.html')
 
 @app.route('/y_predict', methods=['POST'])
 def y_predict():
     x_test = [[float(x) for x in request.form.values()]]
-    print('actual', x_test)
-    x_test = trans.transform(x_test)
-    print('transformed', x_test)
-    pred = model.predict(x_test)
+    # print('actual', x_test)
+    x_test = x_trans.transform(x_test)
+    # print('transformed', x_test)
+    pred_scaled = model.predict(x_test)
+    # print(pred_scaled)
+    pred_scaled_2d = np.array(pred_scaled).reshape(-1, 1)
+    # print(pred_scaled_2d)
+    pred_celsius = y_trans.inverse_transform(pred_scaled_2d)
+    # print(pred_celsius)
+    pred_celsius_value = float(pred_celsius[0][0])
+    # print(pred_celsius_value)
 
-    return render_template('Manual_predict.html', prediction_text=('Permanent Magnet surface temperature: ', pred[0]))
+    return render_template('result.html', prediction_text=('Permanent Magnet surface temperature: ', pred_celsius_value))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
