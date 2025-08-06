289ac4ce9e4828ba9a64b67d999eb6baab5b410b
Jayesh Prajapati <prajapatijayesh369@gmail.com>
2025-08-06 21:13:57 +0530

    Update app.py

--- Flask Application/app.py
+++ Flask Application/app.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import joblib
 
 app = Flask(__name__)
-model = joblib.load("Model Building\decision_tree_model.save")
-x_trans = joblib.load("Model Building\mm_x_scaler.save")
-y_trans = joblib.load("Model Building\mm_y_scaler.save")
+model = joblib.load("D:\Jayesh\Git\GitHub repos\Electric-Motor-Temperature-Prediction\Model Building\decision_tree_model.save")
+x_trans = joblib.load("D:\Jayesh\Git\GitHub repos\Electric-Motor-Temperature-Prediction\Model Building\mm_scaler_x.save")
+y_trans = joblib.load("D:\Jayesh\Git\GitHub repos\Electric-Motor-Temperature-Prediction\Model Building\mm_scaler_y.save")
 
 app = Flask(__name__)
 
@@ -16,19 +17,14 @@ def predict():
 @app.route('/y_predict', methods=['POST'])
 def y_predict():
     x_test = [[float(x) for x in request.form.values()]]
-    # print('actual', x_test)
     x_test = x_trans.transform(x_test)
-    # print('transformed', x_test)
     pred_scaled = model.predict(x_test)
-    # print(pred_scaled)
-    pred_scaled_2d = np.array(pred_scaled).reshape(-1, 1)
-    # print(pred_scaled_2d)
-    pred_celsius = y_trans.inverse_transform(pred_scaled_2d)
-    # print(pred_celsius)
-    pred_celsius_value = float(pred_celsius[0][0])
-    # print(pred_celsius_value)
+    pred_scaled = np.array(pred_scaled).reshape(-1, 1) 
+    pred_celsius = y_trans.inverse_transform(pred_scaled)
+    pred_value = float(pred_celsius[0][0])
+    result_text = f"Permanent Magnet surface temperature: {pred_value:.2f} °C"
 
-    return render_template('result.html', prediction_text=('Permanent Magnet surface temperature: ', pred_celsius_value))
+    return render_template('result.html', prediction_text=result_text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
